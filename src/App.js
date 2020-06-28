66092a0a4fee0bfca876f846c2267919e0b80501
ahmedraza17260 <ahmedraza17260@gmail.com>
2020-06-28 16:23:38 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./App.css";
+import CountryList from "./components/CountryList/CountryList";
+import SearchBox from "./components/SearchBox/SearchBox";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      countries: [],
+      stats: [],
+      searchField: "",
+    };
+  }
+  async componentDidMount() {
+    const resp = await fetch("https://api.covid19api.com/countries");
+    const countries = await resp.json();
+    this.setState({ countries });
+    this.state.countries.forEach(async (country) => {
+      const resp = await fetch(
+        `https://api.covid19api.com/total/country/${country.Slug}`
+      );
+      const data = await resp.json();
+      if (data.length)
+        this.setState((prevState) => ({
+          stats: prevState.stats.concat({
+            ...data[data.length - 1],
+            CountryCode: country.ISO2,
+          }),
+        }));
+    });
+  }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const { stats, searchField } = this.state;
+    const filteredCountries = stats.filter((country) =>
+      country.Country.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <div className=" box_shadow">
+          <h1>Covid19 App</h1>
+        </div>
+        <SearchBox
+          placeholder="Enter Country Name ..."
+          handleChange={this.handleChange}
+        />
+        <CountryList stats={filteredCountries} />
+
+        <div className="copyRight">
+          <h4> Made By Ahmed Raza </h4>
+          <a href="https://github.com/ahmedraza17260" target="_blank">
+            {" "}
+            <h3> &copy; Ahmed Raza </h3>{" "}
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
